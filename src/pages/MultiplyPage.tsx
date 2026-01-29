d3ee0e050a75eeefe0aa97619f2eb74451c8ff9f
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-01-29 09:08:25 -0500

    cambios

--- src/pages/MultiplyPage.tsx
+++ src/pages/MultiplyPage.tsx
@@ -34,6 +34,56 @@ export default function MultiplyPage() {
       <Typography sx={{ mt: 1 }}>
         Resultado: <strong>{result}</strong>
       </Typography>
+
+      <Typography variant="h5" fontWeight={900} gutterBottom>
+        2. Calcular IVA (12%)
+      </Typography>
+
+      <TextField
+        label="Precio del Producto:"
+        type="number"
+        value={a}
+        onChange={(e) => setA(Number(e.target.value))}
+        sx={{ mr: 2, mb: 2 }}
+      />
+
+      <Typography sx={{ mt: 1 }}>
+        calcular IVA <strong>{result}</strong>
+      </Typography>
+
+
+      <Typography variant="h5" fontWeight={900} gutterBottom> 
+        <h3>3. Calcular Precio con Descuento</h3>
+      </Typography>
+
+      <TextField
+        label="Precio Original:"
+        type="number"
+        value={a}
+        onChange={(e) => setA(Number(e.target.value))}
+        sx={{ mr: 2, mb: 2 }}
+      />
+
+      <TextField
+        label="Porcentaje de Descuento:"
+        type="number"
+        value={b}
+        onChange={(e) => setB(Number(e.target.value))}
+        sx={{ mb: 2 }}
+      />
+
+      <Typography sx={{ mt: 1 }}>
+        Cálculo final: <strong>{result}</strong>
+      </Typography>
+
+
+
+
+
+
+
+
+
     </Paper>
   );
 }
